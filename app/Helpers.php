d5a966d80220a86c2176fb37b89f7c0569229f0c
Yusuf Damiloju <yusufdamiloju@gmail.com>
2018-02-16 04:00:10 +0100

    Add Success and failure helpers

--- app/Helpers.php
+++ app/Helpers.php
@@ -19,3 +19,36 @@ function flash($title = null, $message = null)
 
     return $flash->info($title, $message);
 }
+
+function apiSuccess(string $msg, $data, array $meta=[], bool $sendUserInfo = true)
+{
+
+    $responder = config('app.apiResponse');
+    $responder['status'] = 0;
+    $responder['message'] = $msg;
+    $responder['data'] = $data;
+
+    return response()->json( $responder );
+}
+
+/**
+ * A blueprint of a failed ajax request
+ * @param $msg
+ * @param $data
+ * @param int $code
+ * @return \Illuminate\Http\JsonResponse
+ */
+function apiFailure(string $msg, $data, int $code = 2)
+{
+    /**
+     * 4 => validation error
+     * 2 => internal error
+     * -2 => unauthorized for action
+     */
+    $responder = config('app.apiResponse');
+    $responder['status'] =  $code;
+    $responder['message'] = $msg;
+    $responder['data'] = $data;
+
+    return response()->json($responder);
+}
