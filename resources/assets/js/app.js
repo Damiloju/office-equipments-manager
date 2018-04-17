f2dcd6a612c6669418eabb9cbcb175806d43ed1a
TimothyMee <timothy33.tf@gmail.com>
2018-04-17 15:22:29 +0100

    still working on new UI

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,14 +1,20 @@
 import VueRouter from 'vue-router';
 
 import Vue from 'vue';
+import axios from 'axios';
 import Example from './components/Example';
 import Home from './components/Home';
 import Offices from './components/Offices/Offices';
 import ViewOffice from './components/Offices/ViewOffice';
 import Office from './components/Record/Office';
 import BtnLoader from './misc/ButtonLoader.vue';
+import Dashboard from './components/Dashboard.vue';
+import VueSweetAlert from 'vue-sweetalert'
 
+
+Vue.use(VueSweetAlert);
 Vue.use(VueRouter);
+Vue.use(axios);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -20,6 +26,7 @@ Vue.component('viewoffice', ViewOffice);
 Vue.component('home', Home);
 Vue.component('office', Office);
 Vue.component('btn-loading', BtnLoader);
+Vue.component('dashboard', Dashboard);
 
 const app = new Vue({
     el: '#app'
